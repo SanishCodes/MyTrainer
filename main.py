e447326659a2d66394a56b32f735e1887ca84412
Sanish Shrestha <sanishshrestha@Sanishs-MacBook-Pro.local>
2025-07-18 15:07:08 +1000

    Added pushup counter and updated main file

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from app.models.biceps_curl_counter import BicepCurlCounter
-from app.models.squats_counter import SquatsCounter
+from app.models import BicepCurlCounter, SquatsCounter, PushupCounter, CrunchCounter, OverheadTricepsCounter, DeadliftCounter, LateralRaiseCounter, BarbellRowCounter, LungesCounter
+
+
 
 def main():
    
-    print("Choose exercise: 1) Bicep Curl  2) Squats")
-    choice = input("Enter your choice (1/2): ")
+    print("Choose exercise: 1) Bicep Curl  2) Squats 3) Pushup")
+    choice = input("Enter your choice (1-3): ")
 
     if choice == '1':
         biceps_counter = BicepCurlCounter()
         
     elif choice == '2':
         squats_counter = SquatsCounter()
-        
+    
+    elif choice == '3':
+        PushupCounter()
+    
+    
     else:
         print("Invalid choice. Exiting.")
 
 if __name__ == "__main__":
     main()
+   
