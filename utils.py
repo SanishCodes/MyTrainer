b3793a8f21ded5975d1a803a7985cb205cf8baef
Sanish Shrestha <sanishshrestha@Sanishs-MacBook-Pro.local>
2025-07-18 14:31:36 +1000

    Updated calculated_angle func with dot product method

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 import numpy as np
 import cv2
 
+
 def calculate_angle(a, b, c):
     a, b, c = np.array(a), np.array(b), np.array(c)
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians * 180.0 / np.pi)
+    angle = round(np.abs(radians * 180.0 / np.pi),0)
     return 360 - angle if angle > 180 else angle
+
+
+
 
 def show_angle(image, angle, reference_point):
     cv2.putText(image, str(angle),
